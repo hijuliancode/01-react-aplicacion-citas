6ea73f85d03265378ce3d7cc223125806c0334f4
JulianCode <hijuliansosa@protonmail.com>
2020-06-11 13:56:34 -0500

    formulario markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <h2>Administrador de pacientes</h2>
+    <Fragment>
+      <h2>Administrador de pacientes</h2>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">b</div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
