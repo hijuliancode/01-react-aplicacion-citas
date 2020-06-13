335d934b2981794e3c7fbcdb3193b763ebbaa679
JulianCode <hijuliansosa@protonmail.com>
2020-06-13 00:20:10 -0500

    El hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from "./components/Formulario";
 import Cita from "./components/Cita";
 
 function App() {
   // Lista de citas
   const [citas, guardarCitas] = useState([])
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+
+  }, [citas])
 
   // Lee las citas actuales y agrega la nueva
   const crearCita = cita => {
